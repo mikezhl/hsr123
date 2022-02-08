0dc111f4aa35a838b0b8029446a3dfd69cb8468c
Haolang Zhu <1447143618@qq.com>
2022-02-08 21:24:53 +0000

    tidy up

--- src/pickup_follow.py
+++ src/pickup_follow.py
@@ -17,6 +17,7 @@ dt=0.15
 vel_limit = 0.1
 start=[-1,-2,0.70558]
 # Initialize
+print("Initializing")
 try:
     robot = hsrb_interface.Robot()
     whole_body = robot.get('whole_body')
@@ -28,36 +29,35 @@ except:
 cli_arm, cli_base = my_Simple_Action_Clients()
 
 # Constrct base list of rrt part1
-print("rrt part1")
+print("Constrcting TrajectoryPoint list for rrt part1")
 KDLFrame_startbase = exo.KDLFrame([start[0],start[1],0,0,0,start[2]])
 base_list = np.array([my_traj_transform(traj_rrt1[i], KDLFrame_startbase) for i in range(len(traj_rrt1))])
 p_base_list_1 = base.prepare_rrt(base_list,vel_limit)
 # Constrct base list of aico part
-print("aico part")
+print("Constrcting TrajectoryPoint list for aico part")
 time_list = np.arange(0.0,dt*len(traj_aico),dt)
 arm_list = traj_aico[:,3:8]
 arm_list = np.c_[arm_list,time_list]
 arm_list = arm_list[1::]
 p_arm_list1 = arm.prepare_aico(arm_list,dt)
-
 traj_aico_base = traj_aico[:,0:3]
 base_list = np.array([my_traj_transform(traj_aico_base[i], KDLFrame_startbase) for i in range(len(traj_aico_base))])
 base_list = np.c_[base_list, time_list]
 base_list = base_list[1::]
 p_base_list_2 = base.prepare_aico(base_list,dt)
 # Constrct base list of rrt part2
-print("rrt part2")
+print("Constrcting TrajectoryPoint list for  rrt part2")
 base_list = np.array([my_traj_transform(traj_rrt2[i], KDLFrame_startbase) for i in range(len(traj_rrt2))])
 p_base_list_3 = base.prepare_rrt(base_list,vel_limit)
 
 # Run!!
 rospy.set_param('pickup_status', 0)
-print("loading base goal1")
+print("Loading base goal1")
 base.load_base_goal_pickup(p_base_list_1,cli_base)
 while rospy.get_param("pickup_status")==0:
     rospy.sleep(0.1)
 
-print("loading base goal2")
+print("Loading base goal2")
 base.load_base_goal_pickup(p_base_list_2,cli_base)
 arm.load_arm_goal_pickup(p_arm_list1,cli_arm)
 rospy.sleep(4*dt/0.1)
@@ -65,13 +65,13 @@ hsrb_gripper.apply_force(0.2, delicate = True)
 while rospy.get_param("pickup_status")<3:
     rospy.sleep(0.1)
 
-print("loading base goal3")
+print("Loading base goal3")
 base.load_base_goal_pickup(p_base_list_3,cli_base)
 while rospy.get_param("pickup_status")==3:
     rospy.sleep(0.1)
 
 print('All Done :)')
-
+rospy.sleep(0.5)
 
 
 
